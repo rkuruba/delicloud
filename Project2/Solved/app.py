34943cc88e4834d050d332de304123af4f3895a5
flexdog <fbarron@gmail.com>
2020-07-09 12:52:31 -0400

    Upload latest changes

--- Project2/Solved/app.py
+++ Project2/Solved/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, jsonify
+
 
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
 import pymongo
@@ -30,6 +31,24 @@ def index():
     # Return the template with the teams list passed in
     return render_template('index.html', productlist=productlist)
 
+# Set route for table
+@app.route('/index3.html')
+def index3():
+    # Store the entire team collection in a list
+    productlist3 = list(db.products.find())
+    print(productlist3)
+
+    # Return the template with the teams list passed in
+    return render_template('index3.html', productlist3=productlist3)
+
+@app.route('/jsonData',methods=["GET","POST"])
+def jsonData():
+    # Store the entire team collection in a list
+    productlist3 = list(db.products.find())
+    response = Response(response=productlist3, status=200, mimetype="application/json")
+
+    # Return the template with the teams list passed in
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
